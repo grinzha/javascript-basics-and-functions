ca50807764c6273b6cc1a37f5981f499110c46d1
IlyaGrinzhilevskiy <ilyagrinzhilevski@gmail.com>
2023-06-10 01:19:11 +0300

    001-sum

--- problems/002-basketball.js
+++ problems/002-basketball.js
@@ -16,7 +16,32 @@
  * @returns {(number|undefined)}
  */
 function getWinner(points) {
-    return undefined;
+    let firstTeamPoints = [];
+    let secondTeamPoints = [];
+
+    points.map(item => {
+        firstTeamPoints.push(item.slice(0, 2));
+        secondTeamPoints.push(item.slice(3, 5));
+    })
+
+    let firstTeamSummary = 0;
+    firstTeamPoints.forEach(item => {
+        firstTeamSummary += +item;
+    });
+
+    let secondTeamSummary = 0;
+    secondTeamPoints.forEach(item => {
+        secondTeamSummary += +item;
+    });
+
+    if (firstTeamSummary === secondTeamSummary) {
+        return undefined;
+    }
+
+    return firstTeamSummary > secondTeamSummary ? 1 : 2;
 }
 
-module.exports = getWinner;
+
+
+
+module.exports = getWinner(['23-26', '24-30', '30-27', '35-31']);
